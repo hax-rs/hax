ca02d4297cc99f1e95458f2ae3faf1d4a8a14a78
7ap <109433659+7ap@users.noreply.github.com>
2022-11-28 07:32:39 -0600

    feat(hax-macros): add hax::main macro

---
+++ hax-macros/src/entry.rs
@@ -0,0 +1,53 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
+
+pub(crate) fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // let attr = parse_macro_input!(attr as AttributeArgs);
+    // let item = parse_macro_input!(item as Item);
+
+    let ast = parse_macro_input!(item as ItemFn);
+
+    let stmts = &ast.block.stmts;
+
+    let expanded = quote! {
+        #[link(name = "kernel32")]
+        extern "system" {
+            fn FreeLibraryAndExitThread(module: usize, exit_code: u32) -> !;
+        }
+
+        #[no_mangle]
+        unsafe extern "system" fn DllMain(module: usize, reason: u32, _: usize) -> isize {
+            if reason == 1 {
+                std::thread::spawn(move || unsafe {
+                    #(#stmts)*
+
+                    FreeLibraryAndExitThread(module as _, 0);
+                });
+
+                return 1;
+            }
+
+            0
+        }
+    };
+
+    println!("{}", expanded);
+
+    expanded.into()
+}
+
+// #[no_mangle]
+// unsafe extern "system" fn DllMain(module: usize, reason: u32, _: usize) -> isize {
+//     if reason == 1 {
+//         std::thread::spawn(move || unsafe {
+//             // ...
+
+//             FreeLibraryAndExitThread(module as _, 0);
+//         });
+
+//         return 1;
+//     }
+
+//     0
+// }
--- hax-macros/src/lib.rs
+++ hax-macros/src/lib.rs
@@ -1,1 +1,8 @@
+mod entry;
 
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
+    entry::main(attr, item)
+}

6e6a1e3cbdc97e5b1b8105af5656cfa709506e8e
7ap <109433659+7ap@users.noreply.github.com>
2022-11-28 11:53:32 -0600

    feat(hax): add macro_rules! main macro

--- hax/src/lib.rs
+++ hax/src/lib.rs
@@ -1,1 +1,35 @@
-pub use hax_macros::*;
+#[cfg(feature = "external")]
+#[macro_export]
+macro_rules! main {
+    ($function:expr) => {
+        fn main() {
+            $function();
+        }
+    };
+}
+
+#[cfg(feature = "internal")]
+#[macro_export]
+macro_rules! main {
+    ($function:expr) => {
+        #[link(name = "kernel32")]
+        extern "system" {
+            fn FreeLibraryAndExitThread(module: usize, exit_code: u32) -> !;
+        }
+
+        #[no_mangle]
+        unsafe extern "system" fn DllMain(module: usize, reason: u32, _: usize) -> bool {
+            if reason == 1 {
+                std::thread::spawn(move || unsafe {
+                    $function();
+
+                    FreeLibraryAndExitThread(module, 0);
+                });
+
+                return true;
+            }
+
+            false
+        }
+    };
+}
